e895b39c2d2fae4472a5d23ac5aeff13dd9802e1
jon manes <jon.manes@gmail.com>
2016-07-14 21:31:39 -0500

    Assignment 9 DOM Scripting

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,22 +1,19 @@
  var pointsArray = document.getElementsByClassName('point');
  
- var animatePoints = function(points) {
-     
-                 for (var i = 0; i < points.length; i++){
- 
-                 var revealPoint = function() {
-                     points[i].style.opacity = 1;
-                     points[i].style.transform = "scaleX(1) translateY(0)";
-                     points[i].style.msTransform = "scaleX(1) translateY(0)";
-                     points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+ var revealPoint = function(points) {
+                     points.style.opacity = 1;
+                     points.style.transform = "scaleX(1) translateY(0)";
+                     points.style.msTransform = "scaleX(1) translateY(0)";
+                     points.style.WebkitTransform = "scaleX(1) translateY(0)";
                      
-                 }
-     
-                 revealPoint();
-
-                 }
+                 };
  
-             }
+ var animatePoints = function(points) {
+    
+     forEach(points, revealPoint);
+                
+     };
+
  window.onload = function() {
      
      if (window.innerHeight > 950 ) {
---
+++ scripts/utilities.js
@@ -0,0 +1,9 @@
+
+function forEach(points, call) {
+    
+     for (var i = 0; i < points.length; i++) {
+         
+         call(points[i]);
+         
+     }
+}
