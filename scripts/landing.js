e28656fea4449f98ebe12665ea31460432ed88d0
jon manes <jon.manes@gmail.com>
2016-07-14 20:49:35 -0500

    Checkpoint 9 DOM Scripting

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,8 @@
- var animatedPoints= function() {
+ var pointsArray = document.getElementsByClassName('point');
  
-                 var points = document.getElementsByClassName('point');
+ var animatePoints = function(points) {
      
-                 for (var i = 0; i < points.length; i++){}
+                 for (var i = 0; i < points.length; i++){
  
                  var revealPoint = function() {
                      points[i].style.opacity = 1;
@@ -10,11 +10,31 @@
                      points[i].style.msTransform = "scaleX(1) translateY(0)";
                      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
                      
-                 };
+                 }
      
                  revealPoint();
 
-                 };
+                 }
  
-             };
+             }
+ window.onload = function() {
+     
+     if (window.innerHeight > 950 ) {
+         
+         animatePoints(pointsArray);
+         
+     }
+     
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+     
+     window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             
+            animatePoints(pointsArray);
+             
+         }
+     });
+     
+ }
              
