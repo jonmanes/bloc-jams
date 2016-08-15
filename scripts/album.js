378d6163f16f89efa93851329cb9481c540226b6
jon manes <jon.manes@gmail.com>
2016-08-14 21:03:12 -0500

    Assignment-20

--- scripts/album.js
+++ scripts/album.js
@@ -253,6 +253,21 @@ var previousSong = function() {
     $lastSongNumberCell.html(lastSongNumber);
     
 };
+
+var togglePlayFromPlayerBar = function() {
+    if (currentSoundFile.isPaused()) {
+        var $playSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
+        $playSongNumberCell.html(pauseButtonTemplate);
+        $('.main-controls .play-pause').html(playerBarPauseButton);
+        currentSoundFile.play();
+    } else if (currentSoundFile) {
+        var $pauseSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
+        $pauseSongNumberCell.html(playButtonTemplate);
+        $('.main-controls .play-pause').html(playerBarPlayButton);
+        currentSoundFile.pause();
+    }
+};
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
@@ -265,11 +280,13 @@ var currentSoundFile = null;
 var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
-var $nextButton = $('.main-controls .next')
+var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 
 $(document).ready(function() {
   setCurrentAlbum(albumPicasso);  
   $previousButton.click(previousSong);
   $nextButton.click(nextSong);    
+  $playPauseButton.click(togglePlayFromPlayerBar);
 });
